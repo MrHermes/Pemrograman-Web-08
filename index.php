61b2cff42f14fb1094a727e4ffdedcc7bd44e396
Rencist <dafianakhdan@gmail.com>
2023-04-29 20:13:56 +0700

    feat: config php template and sql migration

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Silahturami Online</title>
+  </head>
+  <body>
+    <main>
+      <div class="login">
+        <h1>Masuk<br />Silahturami Online</h1>
+        <form>
+          <label for="email">Email</label>
+          <input type="email" name="email" placeholder="Masukan Email" />
+
+          <label for="password">Kata Sandi</label>
+          <input type="password" name="password" placeholder="Masukan Kata Sandi" />
+
+          <input class="submit" type="submit" value="Masuk" name="masuk" />
+        </form>
+        <h6>Warga baru Silahturami Online? <a href="./form-daftar.php">Daftar</a></h6>
+      </div>
+    </main>
+  </body>
+</html>
---
+++ main.sql
@@ -0,0 +1,35 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2023-04-29 11:54:36.615
+
+-- tables
+-- Table: user
+CREATE TABLE user (
+    id int  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(128)  NOT NULL,
+    no_telp varchar(128)  NOT NULL,
+    address varchar(128)  NOT NULL,
+    email varchar(128)  NOT NULL,
+    password varchar(128)  NOT NULL,
+    is_admin bool  NOT NULL
+);
+
+-- Table: pesan
+CREATE TABLE pesan (
+    id int  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id int  NOT NULL,
+    pesan varchar(128)  NOT NULL,
+    balasan varchar(128)  NULL
+);
+
+-- foreign keys
+-- Reference: pesan_user (table: pesan)
+ALTER TABLE pesan ADD CONSTRAINT pesan_user FOREIGN KEY pesan_user (user_id)
+    REFERENCES user (id);
+
+-- insert user account
+INSERT INTO user (name, no_telp, address, email, password, is_admin) VALUES
+('Admin', '085161397832', 'Keputih Tegal Timur 2', 'admin@gmail.com', 'Admin12345', true),
+('User', '085161397832', 'Keputih Tegal Timur 2', 'user@gmail.com', 'User12345', false);
+
+-- End of file.
+
